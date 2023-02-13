b5410c92104cd77a655cdcb048d6f8cf9715bef6
Michel Succar Medina <m.succar@uniandes.edu.co>
2023-02-13 15:39:36 +0100

    Add docstring documentation

--- __main__.py
+++ __main__.py
@@ -3,6 +3,17 @@ from typing import Dict, List
 from .dep_graph import read_dep_file, construct_graph, Node
 
 def print_graph(nodes: List[Node], count_ind: int = 0):
+    """
+    Prints the graph in a recursive matter that makes sure the whole tree is printed
+    
+    Parameters
+    __________
+    nodes: List[Node]
+        A list of all the packages mapped in the graph in Node representation
+    count_ind: int
+        The depth of dependency of a package, every time a package calls a dependancy
+        the indentetion of the printing is incremented.
+    """
     for node in nodes:
         print(f"{' '*count_ind}-{node.name}")
         if node.children:
@@ -10,8 +21,13 @@ def print_graph(nodes: List[Node], count_ind: int = 0):
 
 
 if __name__ == '__main__':
+    """
+    Executes the module and creates the dependency graph 
+    """
     dependencies = read_dep_file('test.json')
     
+    # We check if the dependencies are not reduntant so there is not an infinite recurtion in the
+    # printing of the graph
     if all(dependencies.values()):
         print("No redundant dependencies found")
     else:
--- dep_graph.py
+++ dep_graph.py
@@ -1,7 +1,25 @@
+
 import json
 from typing import List, Dict, Union
 
 class Node:
+    """
+    A class to represent a node within the Dependecies graph.
+
+    ...
+
+    Attributes
+    __________
+    name: str
+        Name of the package represented by the node
+    children: List[Node]
+        List of all the packages the current package depends on,
+        this  will be representes as Nodes themselfs
+
+    Methods
+    _______
+
+    """
     def __init__(self, name: str, children: List['Node'] = None):
         self.name = name
         self.children = children or []
@@ -10,12 +28,30 @@ class Node:
         return f"Node({self.name})"
 
 def read_dep_file(filename: str) -> Dict[str, List[str]]:
+    """
+    Reads the json file passed to the module
+
+    Parameters
+    __________
+        filename: str
+            path to the file containing the package list and their 
+            dependencies
+    """
     with open(filename, 'r') as file:
         dep_file = json.load(file)
     
     return dep_file
 
 def construct_graph(dependencies: Dict[str, List[str]]) -> Dict[str, Node]:
+    """
+    Cunstructs the graph of dependancies
+
+    Parameters
+    __________
+        dependencies: Dict[str, List[str]]
+            A dictionary that represents the list of packages and a list of
+            their dependencies
+    """
     graph = {}
     for pkg, children in dependencies.items():
         if pkg not in graph:
